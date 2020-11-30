3db4f3498aea72cd0f1d4b4a5954d98a2a541c4a
pitchounvivi <pitchounvivi@gmail.com>
2020-11-30 11:35:17 +0100

    Affichage du compte des Employés (liaison avec Employerepository)

--- src/main/java/com/ipiecoles/java/java320/controller/AccueilController.java
+++ src/main/java/com/ipiecoles/java/java320/controller/AccueilController.java
@@ -1,2 +1,22 @@
-package com.ipiecoles.java.java320.controller;public class AccueilController {
+package com.ipiecoles.java.java320.controller;
+
+import com.ipiecoles.java.java320.repository.EmployeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccueilController {
+
+    @Autowired
+    private EmployeRepository employeRepository;
+
+    // Pour test : http://localhost:8080
+    @GetMapping(value = "/")
+    public String index(final ModelMap model){
+        model.put("count", employeRepository.count());
+        return "accueil";
+    }
+
 }
