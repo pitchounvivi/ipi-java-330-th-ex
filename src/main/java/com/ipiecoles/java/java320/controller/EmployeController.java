e41d88d094d33e0e1b1baecf7d8a3282f011100c
pitchounvivi <pitchounvivi@gmail.com>
2020-11-30 14:27:21 +0100

    Recherche employé par matricule + petit correction dans le @RequestMapping

--- src/main/java/com/ipiecoles/java/java320/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/java320/controller/EmployeController.java
@@ -3,12 +3,12 @@ package com.ipiecoles.java.java320.controller;
 import com.ipiecoles.java.java320.model.Employe;
 import com.ipiecoles.java.java320.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -19,10 +19,10 @@ public class EmployeController {
     @Autowired
     private EmployeRepository employeRepository;
 
+    //Recherche d'un employé par son id
     @RequestMapping(
             value = "/{id}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+            method = RequestMethod.GET)
     public String getEmployeById(
             final ModelMap model,
             @PathVariable Long id
@@ -34,5 +34,20 @@ public class EmployeController {
         model.put("employe", employeOptional.get());
         return "detail";
     }
+
+
+    //Recherche d'un employé par son matricule
+    @RequestMapping(
+            params = {"matricule"},
+            method = RequestMethod.GET)
+    public String searchByMatricule(
+            final ModelMap model,
+            @RequestParam(value = "matricule") String matricule
+    ){
+        Employe employe = employeRepository.findByMatricule(matricule);
+        model.put("employe", employe);
+        return "detail";
+    }
+
 
 }
