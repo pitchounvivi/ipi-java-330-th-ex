1a1965bcf050aab88e174fc2b86be8442a31bcbe
pitchounvivi <pitchounvivi@gmail.com>
2020-11-30 15:01:30 +0100

    Liste paginée de tous les employés + petites corrections

--- src/main/java/com/ipiecoles/java/java320/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/java320/controller/EmployeController.java
@@ -4,6 +4,8 @@ import com.ipiecoles.java.java320.model.Employe;
 import com.ipiecoles.java.java320.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -38,15 +40,37 @@ public class EmployeController {
 
     //Recherche d'un employé par son matricule
     @RequestMapping(
-            params = {"matricule"},
+            params = "matricule",
             method = RequestMethod.GET)
     public String searchByMatricule(
             final ModelMap model,
             @RequestParam(value = "matricule") String matricule
     ){
         Employe employe = employeRepository.findByMatricule(matricule);
+
+        //gérer 404
+
         model.put("employe", employe);
         return "detail";
+    }
+
+
+    //Liste des employés
+    @RequestMapping(
+            params = {"page","size","sortProperty","sortDirection"},
+            method = RequestMethod.GET
+    )
+    public String listDeTousLesEmployes(
+            final ModelMap model,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(defaultValue = "name") String sortProperty,
+            @RequestParam(value = "sortDirection", defaultValue = "ASC") String sortDirection
+    ){
+        Page<Employe> pageEmployes = employeRepository.findAll(PageRequest.of(page, size,
+                Sort.Direction.fromString(sortDirection), sortProperty));
+        model.put("employes", pageEmployes);
+        return "listeEmployes";
     }
 
 
